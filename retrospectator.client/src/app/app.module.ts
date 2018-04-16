f0a13310ea7ac58ebaac50bb125bd773b811c26d
MaksymBe <maksymbe@interlink-ua.com>
2018-04-16 18:45:16 +0300

    add some components

--- retrospectator.client/src/app/app.module.ts
+++ retrospectator.client/src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthComponent } from './auth/auth.component';
+import { RetroComponent } from './dashboard/retro/retro.component';
+import { RetroHistoryComponent } from './dashboard/retro-history/retro-history.component';
+import { SprintComponent } from './dashboard/sprint/sprint.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AuthComponent,
+    RetroComponent,
+    RetroHistoryComponent,
+    SprintComponent
   ],
   imports: [
     BrowserModule
