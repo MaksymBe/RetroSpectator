23c9d90449212284685256e134e49c12d5593251
MaksymBe <maksymbe@interlink-ua.com>
2018-04-16 16:15:02 +0300

    first commit

---
+++ retrospectator.server/src/main/java/com/dimax/retrospectator/RetrospectatorApplication.java
@@ -0,0 +1,12 @@
+package com.dimax.retrospectator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrospectatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetrospectatorApplication.class, args);
+	}
+}
