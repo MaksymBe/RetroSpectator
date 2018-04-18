c6c1d8e7db782dad584e85753f505ef2c287d757
MaksymBe <maksymbe@interlink-ua.com>
2018-04-18 16:43:43 +0300

    little fix

--- retrospectator.client/src/environments/environment.ts
+++ retrospectator.client/src/environments/environment.ts
@@ -9,6 +9,6 @@ export const environment = {
     clientID: '2d3p9j186oMI9xC1Rihg4fkb3PWHM5He',
     domain: 'internin6k.eu.auth0.com',
     callbackURL: 'http://localhost:4200/callback',
-    audience: 'http://localhost:3000'
+    audience: 'https://internin6k.eu.auth0.com/api/v2/'
   }
 };
