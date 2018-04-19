1dccd6c4a289ac2356fa1def842c6607e6db5ed7
intern <intern@intern-pc.interlink.office>
2018-04-19 17:16:13 +0300

    fixing services bugs

--- retrospectator.client/src/app/data-service/services/retro/retro.service.ts
+++ retrospectator.client/src/app/data-service/services/retro/retro.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@angular/core';
+import {of} from 'rxjs/observable/of';
 
 @Injectable()
 export class RetroService {
 
   constructor() { }
 
+  getRetrosByTeam() {
+    return of([]);
+  }
 }
