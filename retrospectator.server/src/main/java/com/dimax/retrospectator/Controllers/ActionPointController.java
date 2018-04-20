f6c5fdc1c3fb7bbf323dec06fd9c3e735b95cf7a
intern <intern@intern-pc.interlink.office>
2018-04-20 15:32:55 +0300

    hot fix

--- retrospectator.server/src/main/java/com/dimax/retrospectator/Controllers/ActionPointController.java
+++ retrospectator.server/src/main/java/com/dimax/retrospectator/Controllers/ActionPointController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/actionpoint")
+@RequestMapping("/action-point")
 @CrossOrigin(origins = "*")
 public class ActionPointController {
     @Autowired
