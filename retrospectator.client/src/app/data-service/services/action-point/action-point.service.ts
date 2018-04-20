740357b515a94ae0a101c5ecf06f17073b7582a6
MaksymBe <maksymbe@interlink-ua.com>
2018-04-20 13:58:33 +0300

    fix action-points service

--- retrospectator.client/src/app/data-service/services/action-point/action-point.service.ts
+++ retrospectator.client/src/app/data-service/services/action-point/action-point.service.ts
@@ -1,18 +1,28 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
 import {APoint} from '../../model/APoint';
+import {environment} from '../../../../environments/environment';
+import {Point} from '../../model/Point';
 
 @Injectable()
 export class ActionPointService {
 
-  constructor(private http: HttpClient) {}
+  private urlModifier = 'action-point';
 
-  getActionPointsByTeam(teamKey: number): Observable<APoint[]> {
-    return <Observable<APoint[]>>this.http.get('http://localhost:3000/' + teamKey + '/action-points');
+  constructor(private http: HttpClient) {
+
   }
 
-  addActionPoint(teamKey: number, actionPoint: APoint): Observable<APoint> {
-    return <Observable<APoint>>this.http.post('http://localhost:3000/' + teamKey + '/action-points', actionPoint);
+  getActionPoints(teamId: string): Observable<any> {
+    return this.http.get(environment.apiHost + `${this.urlModifier}/${teamId}`);
+  }
+
+  createActionPoint(point: APoint, teamId: string): Observable<APoint> {
+    return <Observable<APoint>>this.http.post(environment.apiHost + `${this.urlModifier}/${teamId}`, point);
+  }
+
+  updatePoint(point: APoint): Observable<APoint> {
+    return <Observable<APoint>>this.http.patch(environment.apiHost + `${this.urlModifier}/${point.id}`, point);
   }
 }
